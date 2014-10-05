a699b941ea76be5dd85210e6bd9c9ce1c4880cfa
Loredana Crisan <loredanasf@gmail.com>
2014-10-04 22:47:57 -0700

    Initial Commit

---
+++ Facebook-Transition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facebook-Transition
+//
+//  Created by Loredana Crisan on 10/4/14.
+//  Copyright (c) 2014 Loredana Crisan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Facebook-TransitionTests/Facebook_TransitionTests.swift
@@ -0,0 +1,36 @@
+//
+//  Facebook_TransitionTests.swift
+//  Facebook-TransitionTests
+//
+//  Created by Loredana Crisan on 10/4/14.
+//  Copyright (c) 2014 Loredana Crisan. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Facebook_TransitionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
